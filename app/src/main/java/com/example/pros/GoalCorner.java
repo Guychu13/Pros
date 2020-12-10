129096b1f3220fd7fd29582ba618a55a3f259d6a
Guychu <guygb2000@gmail.com>
2020-12-10 02:06:18 +0200

    There is a CPU enemy but the code is trash and no super Block class

--- app/src/main/java/com/example/pros/GoalCorner.java
+++ app/src/main/java/com/example/pros/GoalCorner.java
@@ -1,4 +1,12 @@
 package com.example.pros;
 
-public class GoalCorner {
+import android.graphics.Bitmap;
+
+public class GoalCorner extends GameObject{
+
+    public GoalCorner(Bitmap bitmap, int xPos, int yPos, int windowWidth, int windowHeight) {
+        super(bitmap, xPos, yPos, windowWidth, windowHeight);
+    }
+
+
 }
